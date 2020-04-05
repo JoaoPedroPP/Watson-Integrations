a5d02b8aa6763ef891778d9c806b125d0aa448f7
JoaoPedroPP <jp052@hotmail.com>
2020-04-04 23:12:55 -0300

    update python with discovery

--- python/app.py
+++ python/app.py
@@ -2,6 +2,7 @@ import json
 import os
 from flask import Flask, render_template, request
 from ibm_watson import NaturalLanguageUnderstandingV1, ApiException
+from ibm_watson import DiscoveryV1
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 from ibm_watson.natural_language_understanding_v1 \
     import Features,\
@@ -15,12 +16,19 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 port = int(os.getenv('PORT', 3000))
 
-authenticator = IAMAuthenticator('APIKEY do seu serviço')
+authenticatorNLU = IAMAuthenticator('APIKEY do seu serviço')
 nlu = NaturalLanguageUnderstandingV1(
     version='2019-07-12',
-    authenticator=authenticator
+    authenticator=authenticatorNLU
 )
-nlu.set_service_url('sua url')
+nlu.set_service_url('url do seu serviço')
+
+authenticatorDiscovery = IAMAuthenticator('APIKEY do seu serviço')
+discovery = DiscoveryV1(
+    version='2019-07-12',
+    authenticator=authenticatorDiscovery
+)
+discovery.set_service_url('url do seu serviço')
 
 def analyzeFrame(text):
     try:
@@ -37,6 +45,16 @@ def analyzeFrame(text):
     except (Exception, ApiException) as err:
         return { 'err': True , 'errMsg': err.__str__() }
 
+def queryDiscovery(query):
+    try:
+        return discovery.query(
+            environment_id='environmentId do seu serviço',
+            collection_id='collectionId do seu serviço',
+            query=query,
+            passages=True).get_result()
+    except (Exception, ApiException) as err:
+        return { 'err': True , 'errMsg': err.__str__() }
+
 @app.route('/nlu-completo', methods=['GET'])
 def nluCompleto():
     text = request.args.get('text')
@@ -45,6 +63,15 @@ def nluCompleto():
         return resp
     else:
         return '<p>Erro: Parametro \'text\' não encontrado</p>'
+
+@app.route('/discovery', methods=['GET'])
+def discoveryCompleto():
+    query = request.args.get('query')
+    if query != None:
+        resp = queryDiscovery(query)
+        return resp
+    else:
+        return '<p>Erro: Parametro \'query\' não encontrado</p>'
 
 @app.route('/', methods=['GET'])
 def main():
