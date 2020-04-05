155500705737ea857803cdefb442ab0dc9e9e77d
JoaoPedroPP <jp052@hotmail.com>
2020-04-04 22:55:26 -0300

    update node with discovery

--- node/app.js
+++ node/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const NLU = require('ibm-watson/natural-language-understanding/v1');
+const Discovery = require('ibm-watson/discovery/v1');
 const { IamAuthenticator } = require('ibm-watson/auth');
 require('dotenv').config()
 
@@ -10,8 +11,16 @@ const nlu = new NLU({
     authenticator: new IamAuthenticator({
         apikey: 'APIKEY do seu serviço'
     }),
-    url: 'sua url'
+    url: 'url do seu serviço'
 });
+
+const discovery = new Discovery({
+    version: '2019-04-30',
+    authenticator: new IamAuthenticator({
+        apikey: 'APIKEY do seu serviço'
+    }),
+    url: 'url do seu serviço'
+})
 
 const nluCompleto = (params) => {
     return new Promise((resolve, reject) => {
@@ -20,6 +29,14 @@ const nluCompleto = (params) => {
             .catch(err => reject(err));
     });
 }
+
+const discoveryCompleto = (params) => {
+    return new Promise((resolve, reject) => {
+        discovery.query(params)
+            .then(resp => resolve(resp))
+            .catch(err => reject(err));
+    });
+};
 
 app.set('port', process.env.PORT || 3000);
 
@@ -47,6 +64,19 @@ app.get('/nlu-completo', (req, res) => {
             console.log(JSON.stringify(err, null, 2));
             res.send({ err: true, errMsg: err });
         })
+});
+
+app.get('/discovery', (req, res) => {
+    discoveryCompleto({
+        query: req.query.query,
+        environmentId: 'environmentId do seu serviço',
+        collectionId: 'collectionId do seu serviço'
+    })
+        .then(data => res.send(data))
+        .catch(err => {
+            console.error(JSON.stringify(err, null, 2))
+            res.send({ err: true, errMsg: err });
+        });
 });
 
 app.get('/', (req, res) => res.send('<h3>Integrando os serviços do Watson</h3>'));
